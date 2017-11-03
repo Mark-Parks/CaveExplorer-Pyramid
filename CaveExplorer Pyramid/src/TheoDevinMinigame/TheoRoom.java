f0efe6714270ea62fce2e212799d02b0802ed4ef
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-03 15:28:34 -0400

    asdf

--- CaveExplorer Pyramid/src/TheoDevinMinigame/TheoRoom.java
+++ CaveExplorer Pyramid/src/TheoDevinMinigame/TheoRoom.java
@@ -3,6 +3,9 @@
  */
 package TheoDevinMinigame;
 
+import java.util.Scanner;
+
+import caveExplorer.CaveExplorer;
 import caveExplorer.NPCRoom;
 
 /**
@@ -11,12 +14,51 @@ import caveExplorer.NPCRoom;
  */
 public class TheoRoom extends NPCRoom {
 
+	private boolean active;
 	/**
 	 * @param description
 	 */
 	public TheoRoom(String description) {
 		super(description);
-		// TODO Auto-generated constructor stub
+		active = true;
 	}
 
+	public String getContents() {
+		if(active) {
+		return "T";
+		}else {
+			return super.getContents();
+		}
+	}
+	public String getDescription() {
+		if(active) {
+		return super.getDescription()+"\n"+"If You want to play a game press 'z'";
+		}else {
+			return super.getDescription();
+		}
+	}
+	public String validMoves() {
+		return "wdsaz";
+	} 
+	public void printValidMoves() {
+		System.out.println("You can only enter 'w','d','s','a' or press 'z' to play a game");
+	}
+	private void performAction(int direction) {
+		if(direction == 4 ) {
+			guessNumber();
+		}
+		else {
+			CaveExplorer.print("That key does nothing OmegaLUL");
+		}
+	}
+
+	private void guessNumber() {
+		String z ="7";
+		Scanner in = new Scanner(System.in);
+		CaveExplorer.print("Guess my random number Xd Kappa haHAA");
+		while(!in.nextLine().equals(z)) {
+			CaveExplorer.print("Thats the wrong number");
+		}
+		CaveExplorer.print("Wow U guessed the right number");
+	}
 }
