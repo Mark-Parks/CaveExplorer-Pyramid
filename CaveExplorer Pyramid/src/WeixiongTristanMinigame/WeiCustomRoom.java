052aa2fba9b3c461f4355153813105d9433e450a
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-11-03 15:32:08 -0400

    asdasf2

--- CaveExplorer Pyramid/src/WeixiongTristanMinigame/WeiCustomRoom.java
+++ CaveExplorer Pyramid/src/WeixiongTristanMinigame/WeiCustomRoom.java
@@ -1,18 +1,29 @@
 package WeixiongTristanMinigame;
 
 import caveExplorer.NPCRoom;
+import java.util.Scanner;
 
 public class WeiCustomRoom extends NPCRoom {
-
+	private Buyables[] shop = new Buyables[1];
+	private static Scanner in;
+	
 	public WeiCustomRoom(String description) {
 		super(description);
-	}
-	
-	public void initializeShop() {
-		Buyables[] shop = new Buyables[6];
+		in = new Scanner(System.in);
+		shop[0] = new Buyables("Potion", 2, 20);
 	}
 	
 	public void enter() {
+		System.out.println("Hello, I am a Moogle, kupo. Here are some items I have on sale. Press 'l' to leave at anytime");
+		startShopping();
+	}
+	
+	private void startShopping() {
+		in = 
+	}
+
+	public void purchaseItem(int itemNum) {
+		Buyables currentItem = shop[itemNum];
 		
 	}
 }
