2591447207427c395b49f8945a73e86571f98099
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-11-03 15:13:01 -0400

    hello

---
+++ CaveExplorer Pyramid/src/WeixiongTristanMinigame/WeiCustomRoom.java
@@ -0,0 +1,18 @@
+package WeixiongTristanMinigame;
+
+import caveExplorer.NPCRoom;
+
+public class WeiCustomRoom extends NPCRoom {
+
+	public WeiCustomRoom(String description) {
+		super(description);
+	}
+	
+	public void initializeShop() {
+		Buyables[] shop = new Buyables[6];
+	}
+	
+	public void enter() {
+		
+	}
+}
