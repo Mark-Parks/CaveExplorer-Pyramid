3b3ca5a25771dee1be16de07bb9ede5c22dc986d
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-03 14:34:46 -0400

    asdf

---
+++ CaveExplorer Pyramid/src/TheoDevinMinigame/TheoRoom.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package TheoDevinMinigame;
+
+import caveExplorer.NPCRoom;
+
+/**
+ * @author BT_1N3_13
+ *
+ */
+public class TheoRoom extends NPCRoom {
+
+	/**
+	 * @param description
+	 */
+	public TheoRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
