ac28db7eaf7523a78ea03cae7ba229f9e6c414e7
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-03 14:57:12 -0400

    lol

---
+++ CaveExplorer Pyramid/src/WeixiongTristanMinigame/TristanFrontEnd.java
@@ -0,0 +1,12 @@
+package WeixiongTristanMinigame;
+
+import caveExplorer.NPCRoom;
+
+public class TristanFrontEnd extends NPCRoom {
+
+	public TristanFrontEnd(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
