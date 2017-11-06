e7ed23cba588d085ea81e9ad6e8e9aad8d1a1457
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-11-06 14:29:05 -0500

    more work to WeiCustomRoom

--- CaveExplorer Pyramid/src/WeixiongTristanMinigame/WeiCustomRoom.java
+++ CaveExplorer Pyramid/src/WeixiongTristanMinigame/WeiCustomRoom.java
@@ -14,12 +14,24 @@ public class WeiCustomRoom extends NPCRoom {
 	}
 	
 	public void enter() {
-		System.out.println("Hello, I am a Moogle, kupo. Here are some items I have on sale. Press 'l' to leave at anytime");
+		System.out.println("Hello, I am a Moogle, kupo. Here are some items I have on sale. Press 'l' to leave at anytime. Press an integer to buy an item in the shop.");
+		printInventory(shop);
 		startShopping();
 	}
 	
+	private void printInventory(Buyables[] shop) {
+		for(int i = 0; i < shop.length; i++) {
+			System.out.println(i + 1);
+		}
+	}
+
 	private void startShopping() {
-		in = 
+		String response = in.nextLine();
+		while(response.toLowerCase().equals("leave")) {
+			if(response > shop.length) {
+				
+			}
+		}
 	}
 
 	public void purchaseItem(int itemNum) {
