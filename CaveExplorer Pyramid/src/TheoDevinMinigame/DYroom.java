4938baa29167ea4069974e3b550fdca5b993b28a
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-09 15:21:10 -0500

    addition

--- CaveExplorer Pyramid/src/TheoDevinMinigame/DYroom.java
+++ CaveExplorer Pyramid/src/TheoDevinMinigame/DYroom.java
@@ -15,7 +15,7 @@ public class DYroom extends NPCRoom {
 	}
 	public void printValidMoves() {
 		System.out.println("You can only enter 'w','d','s', or 'a' or press 'e' to perform an action."
-				+ " You can press b for a description of the room.");
+				+ " You can press b for a chance to get a hint in one of the minigames");
 	}
 	
 	public String getContents() {
@@ -30,6 +30,7 @@ public class DYroom extends NPCRoom {
 	}	
 		public void performAction(int direction) {
 		if(direction == 5) {
+			getNpc().chance();
 			CaveExplorer.print("hi im devin and I dab on haters");
 		}else {
 				CaveExplorer.print("no");
