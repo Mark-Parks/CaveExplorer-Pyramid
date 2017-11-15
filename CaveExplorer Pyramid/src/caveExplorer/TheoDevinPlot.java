cdd6d99e4aa3e2bb3b56793b5d99eafd062292c6
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-15 15:31:31 -0500

    asdf

--- CaveExplorer Pyramid/src/caveExplorer/TheoDevinPlot.java
+++ CaveExplorer Pyramid/src/caveExplorer/TheoDevinPlot.java
@@ -7,8 +7,35 @@ public class TheoDevinPlot {
 	private String value;
 	private int row;
 	private int col;
+	private boolean hasValue;
 	public TheoDevinPlot(int row2, int col2) {
 		this.row = row2;
 		this.col = col2;
+		hasValue = false;
 	}
+	public int getRow() {
+		return row;
+	}
+	public int getCol() {
+		return col;
+	}
+	public String getValue() {
+		return value;
+	}
+	public boolean getUp() {
+		return isUp;
+	}
+	public void setUp(boolean ok) {
+		isUp = ok;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public boolean isHasValue() {
+		return hasValue;
+	}
+	public void setHasValue(boolean hasValue) {
+		this.hasValue = hasValue;
+	}
+
 }
