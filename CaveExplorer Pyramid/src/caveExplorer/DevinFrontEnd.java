9c2795fc8ca778a043db9f028581fc466aa7a534
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-11-15 15:30:49 -0500

    zed is triggered. he very upsetti

--- CaveExplorer Pyramid/src/caveExplorer/DevinFrontEnd.java
+++ CaveExplorer Pyramid/src/caveExplorer/DevinFrontEnd.java
@@ -1,9 +1,12 @@
 package caveExplorer;
 
+import java.util.Scanner;
+
 public class DevinFrontEnd implements TheoSupport {
 	private DevinSupport backend;
 	private boolean first;
 	private int moves;
+	private int matches;
 
 	public static final void main(String args) {
 		DevinFrontEnd demo = new DevinFrontEnd();
@@ -11,8 +14,10 @@ public class DevinFrontEnd implements TheoSupport {
 	public DevinFrontEnd() {
 		backend = new TheoBackEnd(this);
 		moves = 32;
-		first = false;
+		matches = 0;
+		first = false; // 1st tile to be clicked upon is flipped or not flipped
 	}
+	
 	public void GameBegins() {
 		/**
 		 * make board, fill it with "." Each turned down card is a dot
@@ -22,11 +27,15 @@ public class DevinFrontEnd implements TheoSupport {
 		 * 		game 
 		 * }
 		 */
-		while(!backend.isGameOver()) {//when game is still playing {
+		TheoDevinPlot[][]plots = backend.getPlots();//w.e theo calls it
+		TheoDevinPlot L = null;
+		while(!backend.isGameOver()) {//when game is still playing or : while(moves > 0)
 			displayBoard();
 			showMatchNum();
+			System.out.println("Where would you like to press?");
 			TheoDevinPlot okdad = backend.getUserMove();
 			checkMatch(okdad);
+			//moves--;
 		}
 			printMsg(backend.GameoverMsg());	
 	}	
@@ -39,12 +48,34 @@ public class DevinFrontEnd implements TheoSupport {
 		
 	}
 	public void showMatchNum() {
-		
+		System.out.println(matches);
 	}
-	public void displayBoard() {
+	public void displayBoard(TheoDevinPlot[][]plots) {
+		String rows = "0123";
+		String cols = "0123";
+		for(int row = 0; row < plots.length; row++){
+			System.out.print(rows.substring(row, row+1)+" ");
+			for(int col = 0; col < plots[row].length; col++){
+				//if(plots[row][col].isRevealed()){ supposed to be when card is flipped, make conditions with it in showMatchNum?
+					//first = true; 
+					//backend.SymbolsofCards(); reveals the value of cards
+					//else if(backend.isMatch())
+					//{ matches++;
+					// first = false;		}
+
+				
+
+				}else{
+					System.out.print(".");
+				}
+			}
+			System.out.println(" " + rows.substring(row, row+1));
+		}
+		System.out.println(cols.substring(0, plots[0].length+2));
+	//}
 		
-	}
+	//}
 	public void printMsg(String msg){
-		
+		System.out.println(msg);
 	}
 }
