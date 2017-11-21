de041114e93d5ac671ef9b401452f1738101ab87
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-21 14:41:14 -0500

    added minigame room

---
+++ CaveExplorer Pyramid/src/caveExplorer/TheoDevinMinigameRoom.java
@@ -0,0 +1,48 @@
+package caveExplorer;
+
+public class TheoDevinMinigameRoom extends NPCRoom{
+
+	private DevinFrontEnd minigame;
+	
+	public TheoDevinMinigameRoom(String description) {
+		super(description);
+		this.minigame = new DevinFrontEnd();
+	}
+	public void printValidMoves() {
+		CaveExplorer.print("You can only enter 'w','d','s', 'a', or 'e' to perform an action,"
+				+ " or 'f' to play the");
+	}
+	public String validMoves() {
+		return "wdsaef";
+	}
+	public boolean isValid(String input) {
+		return validMoves().indexOf(input) != -1 && input.length() ==1;
+	}
+	public void interpretInput(String input) {
+		while(!isValid(input)) {
+			printValidMoves();
+			input = CaveExplorer.in.nextLine();
+		}
+		int direction = validMoves().indexOf(input);
+		if(direction < 4) {
+			goToRoom(direction);
+		}else {
+			performAction(direction);
+		}
+	}
+	public void performAction(int direction) {
+		if(direction == 5) {
+			minigame.gameBegins();
+		}else {
+			CaveExplorer.print("You should never see this");
+		}
+	}
+	public String getContents() {
+		if(super.getContents() == " ") {
+			return "T";
+		}else {
+			return "T";
+		}
+	}
+
+}
