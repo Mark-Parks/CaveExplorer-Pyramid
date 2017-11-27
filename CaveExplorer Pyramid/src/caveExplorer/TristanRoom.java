47c974c3a4603ac05a665201848c67ddc2ca727b
User <User@Owner-HP>
2017-11-26 23:01:10 -0500

    obelisk

--- CaveExplorer Pyramid/src/caveExplorer/TristanRoom.java
+++ CaveExplorer Pyramid/src/caveExplorer/TristanRoom.java
@@ -11,10 +11,54 @@ public class TristanRoom extends NPCRoom {
 	}
 	
 	public void enter() {
-		super.setContents("There is a fountain in this room. You drink from it and you are"
-				+ "magically healed!");
-		CaveExplorer.inventory.setHp(fullyHealed);
+		super.setDescription("There is an ominous obsidian obelisk in the room.\n"
+				+ "Inscribed with strange, alien sigils, its pointed top nearly touches the ceiling.\n"
+				+ "Press 'f' to approach it.");
 	}
+	
+	public void printValidMoves() {
+		CaveExplorer.print("You can only enter 'w','d','s', 'a', or 'e' to inspect your surroundings,"
+				+ " or 'f' to approach the wall.");
+	}
+	public String validMoves() {
+		return "wdsaef";
+	}
+	
+	private boolean isValid(String input) {
+		return validMoves().indexOf(input) != -1 && input.length() == 1;
+	}
+	public void interpretInput(String input) {
+		while(!isValid(input)){
+			printValidMoves();
+			input = CaveExplorer.in.nextLine();
+		}
+		int direction = validMoves().indexOf(input);
+		if(direction < 4) {
+			goToRoom(direction);
+		}else {
+			performAction(direction);
+		}
+	}
+	public void performAction(int direction) {
+		if(direction == 5) { 
+			if(Math.random() < .3) { 
+				System.out.println("You feel a soothing warmth from the obelisk. + 20 HP."); 
+				CaveExplorer.inventory.setHp(CaveExplorer.inventory.getHp() + 20); 
+			}else if(Math.random() < .3) { 
+				System.out.println("You feel... Nothing. Hm."); 
+			} else if(Math.random() < .3) { 
+				System.out.println("Disappointingly, nothing happens."); 
+			} else { 
+				System.out.println("The obelisk is freezing cold to the touch. \n" 
+				+ "Your surrounings seem to suddenly darken. -25 HP."); 
+				CaveExplorer.inventory.setHp(CaveExplorer.inventory.getHp() - 25); 
+			} 
+			if(CaveExplorer.inventory.getHp() <= 0) {
+				
+			} 
+		} 
+	}
+	
 	
 	public String getContents() {
 		if(super.getContents() == " ") {
@@ -23,5 +67,4 @@ public class TristanRoom extends NPCRoom {
 			return super.getContents();
 		}
 	}
-	
 }
